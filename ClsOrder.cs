9abff863caddb1bcca194425b3865a13471bcf71
Tryals <killerzo@yandex.com>
2023-04-05 12:39:45 +0300

    Добавьте файлы проекта.

---
+++ ClsOrder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class ClsOrder
+    {
+        private string plat;
+        private string sum;
+        private string pol;
+        public string PLAT
+        {
+            get { return plat; }
+            set { plat = value; }
+        }
+        public string POL
+        {
+            get { return pol; }
+            set { pol = value; }
+        }
+        public string SUM
+        {
+            get { return sum; }
+            set { sum = value; }
+        }
+        public ClsOrder()
+        {
+            plat = "";
+            sum = "";
+            pol = "";
+        }
+        public ClsOrder(string f, string g, string p)
+        {
+            plat = f;
+            sum = g;
+            pol = p;
+        }
+        public string PrintToScreen()
+        {
+            string line = $"Расчетный счет плательщика {plat}" + "\n";
+            line += $"Расчетный счет получателя {pol}" + "\n";
+            line += $"Перечисляемая сумма {sum}" + "\n";
+            return line;
+        }
+        public void SaveToFile(string filename)
+        {
+            StreamWriter streamWriter = new StreamWriter(filename, true, Encoding.Default);
+            streamWriter.WriteLine("Данные перевода");
+            streamWriter.WriteLine($"Расчетный счет плательщика: {plat}");
+            streamWriter.WriteLine($"Расчетный счет получателя: {pol}");
+            streamWriter.WriteLine($"Перечисляемая сумма: {sum}");
+            streamWriter.Close();
+        }
+    }
+}
---
+++ Source/Page1.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Source
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        ClsOrder order = new ClsOrder();
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            order.PLAT = PLAT.Text;
+            order.POL = POL.Text;
+            order.SUM = Money.Text;
+
+            ListBox.Items.Clear();
+            ListBox.Items.Add(order.PrintToScreen());
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == false)
+                return;
+            order.SaveToFile(saveFileDialog.FileName);
+        }
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        public void POLBox(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            tb.Text = string.Empty;
+            tb.GotFocus -= POLBox;
+        }
+        public void PLATBox(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            tb.Text = string.Empty;
+            tb.GotFocus -= PLATBox;
+        }
+        public void MoneyBox(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            tb.Text = string.Empty;
+            tb.GotFocus -= MoneyBox;
+        }
+
+    }
+}
